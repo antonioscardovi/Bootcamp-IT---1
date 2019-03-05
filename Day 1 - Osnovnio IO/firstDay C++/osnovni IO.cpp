50ae0fffc5b7b3b08dbb752495fd113071b501be
Antonio Scardovi <antonio.scardovi@gmail.com>
2019-03-05 18:28:37 +0100

    changed folder names, inserted day1

---
+++ Day 1 - Osnovnio IO/firstDay C++/osnovni IO.cpp
@@ -0,0 +1,58 @@
+#include "pch.h";
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	/*
+	// HELLO WORLD
+	cout << "Hello World!" << endl;
+	system("PAUSE");
+	return 0;
+	// KRAJ
+	*/
+
+
+	/*
+	// PRVI ZADATAK
+	int broj1, broj2;
+	std::string ime, prezime;
+
+	cout << "Antonio Scardovi" << endl;
+	cout << "Broj 1: ";
+	cin >> broj1;
+	cout << "Broj 2: ";
+	cin >> broj2;
+	cout << broj1 << " " << broj2 << endl;
+
+	cin.ignore();
+	cout << "Ime : ";
+	getline(cin, ime);
+	cout << "Prezime : ";
+	getline (cin, prezime);
+	cout << "Dobar dan, vi mora da ste: " << ime << " " << prezime << ".\nZadovoljstvo mi je biti vase racunalo.\n" << endl;
+	// KRAJ
+	*/
+
+
+	// DRUGI ZADATAK
+	cout << "x vrijednost \t" << "y vrijednost \t" << "izraz\t\t" << "rezultat\n";
+	cout << "10 \t\t" << "5 \t\t" << "x = y + 3 \tx = 8\n";
+	cout << "10 \t\t" << "5 \t\t" << "x = y - 2 \tx = 3\n";
+	cout << "10 \t\t" << "5 \t\t" << "x = y * 5 \tx = 25\n";
+	cout << "10 \t\t" << "5 \t\t" << "x = x / y \tx = 2\n";
+	cout << "10 \t\t" << "5 \t\t" << "x = x % y \tx = 0\n\n\n" << endl;
+
+	cout << "\t|\t | \n";
+	cout << "\t|\t | \n";
+	cout << "--------------------------\n";
+	cout << "\t|\t | \n";
+	cout << "\t|\t | \n";
+	cout << "--------------------------\n";
+	cout << "\t|\t | \n";
+	cout << "\t|\t | \n";
+	// KRAJ
+
+}
