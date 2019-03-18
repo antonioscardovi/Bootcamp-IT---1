65aedc8b7e675b61d6f01f8c08e72b34726cb2ff
Antonio Scardovi <>
2019-03-18 15:22:57 +0100

    Day 11 - Headeri

---
+++ Day 11 - Headeri/Day 11 - Headeri.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include <iostream>
+#include "operacije.h"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+int zbrajanje(int x, int y) {
+	return x + y;
+}
+
+int oduzimanje(int x, int y) {
+	return x - y;
+}
+
+int mnozenje(int x, int y) {
+	return x * y;
+}
+
+int dijeljenje(double x, double y) {
+	return x / y;
+}
---
+++ Day 11 - Headeri/operacije.h
@@ -0,0 +1,10 @@
+#ifndef OPERACIJE_H
+#define OPERACIJE_H
+
+int zbrajanje(int x, int y);
+int oduzimanje(int x, int y);
+int mnozenje(int x, int y);
+int dijeljenje(double x, double y);
+
+
+#endif
