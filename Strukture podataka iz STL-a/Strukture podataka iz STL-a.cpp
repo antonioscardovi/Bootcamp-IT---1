1b18a0712b10220175ddeda38054012802c72179
Antonio Scardovi <>
2019-03-22 16:08:26 +0100

    Strukture podataka

---
+++ Strukture podataka iz STL-a/Strukture podataka iz STL-a.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void palindrom();
+
+int main()
+{
+	palindrom();
+}
+
+void palindrom() {
+	stack <string> s;
+	char rijeci[20];
+	
+	cout << "Unesi rijec/i: ";
+	cin >> rijeci;
+
+	s.push(rijeci);
+
+	
+
+	
+}
